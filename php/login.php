2574f78becf1f8177443cc2c67e961a5a6ab9947
Vladislav Vasilev <vvasilev1@uclan.ac.uk>
2026-04-24 16:28:38 +0300

    Design for login.php

--- php/login.php
+++ php/login.php
@@ -1,28 +1,156 @@
-<?php include "conn.php"; ?>
+<?php include "conn.php"; 
+    $error = "";
+    if($_SERVER["REQUEST_METHOD"] === "POST"){
+                
+        $email = htmlspecialchars($_POST['email']);
+        $password = $_POST['password'];
 
-<form method="POST">
-    <input type="email" name="email" placeholder="Enter your email" required>
-    <input type="password" name="password" placeholder="Enter your password" required>
-    <button>Login</button>
-</form>
 
-<?php
-if($_SERVER["REQUEST_METHOD"] === "POST"){
+        $stmt = $conn->prepare("SELECT * FROM tbl_users WHERE user_email=?");
+        $stmt->bind_param("s", $email);
+        $stmt->execute();
 
-    $stmt = $conn->prepare("SELECT * FROM tbl_users WHERE user_email=?");
-    $stmt->bind_param("s", $_POST['email']);
-    $stmt->execute();
+        $user = $stmt->get_result()->fetch_assoc();
 
-    $user = $stmt->get_result()->fetch_assoc();
-
-    if($user && password_verify($_POST['password'], $user['user_pass'])){
-        $_SESSION['user_id'] = $user['user_id'];
-        $_SESSION['user'] = $user['user_name'];
-
-        header("Location: index.php");
-        exit;
-    } else {
-        echo "<p class='error'>Wrong email or password</p>";
+        if($user && password_verify($_POST['password'], $user['user_pass'])){
+            $_SESSION['user_id'] = $user['user_id'];
+            $_SESSION['user'] = $user['user_name'];
+            header("Location: index.php");
+            exit;
+        } else {
+            $error = "Wrong email or password";
+        }
     }
-}
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UCLan Shop</title>
+
+    <!-- 
+        Connecting an external CSS file.
+        All styles are separated.
+    -->
+    <link rel="stylesheet" href="../css/style.css">
+
+</head>
+<body>
+    <!-- 
+        HEADER:
+        The semantic element <header> is used
+        to place the logo and navigation.
+    -->
+    <header>
+        <!-- Website logo -->
+        <div class="logo">
+            <img src="../media/images/logo_reverse.png" alt="Logo">
+        </div>
+
+        <!-- Text name of the site -->
+        <div class="header-text">Student Shop</div>
+
+        <!-- Welcome words for logined users -->
+        <?php if(isset($_SESSION['user'])): ?>
+            <span>Welcome, <?php echo $_SESSION['user']; ?></span>
+        <?php endif; ?>
+
+        <!-- 
+            Main navigation for desktop screens
+        -->
+        <nav class="nav-links">
+            <a href="index.php">Home</a>
+            <a href="products.php">Products</a>
+            <a href="cart.php">Cart</a>
+
+            <!-- Extra navigation -->
+            <?php if(isset($_SESSION['user'])): ?>
+                <a href="logout.php">Logout</a>
+            <?php else: ?>
+                <a href="login.php">Login</a>
+            <?php endif; ?>
+        </nav>
+
+        <!-- 
+            Burger menu button for mobile navigation.
+        -->
+        <div class="burger" id="burgerBtn">
+            ☰
+        </div>
+
+        <!-- 
+            Mobile menu.
+            Displayed/hidden when clicking on the burger icon. Hidden when the screen is large enough.
+        -->
+        <nav class="mobile-menu" id="mobileMenu">
+            <a href="index.php">Home</a>
+            <a href="products.php">Products</a>
+            <a href="cart.php">Cart</a>
+
+            <!-- Extra navigation for mobile -->
+            <?php if(isset($_SESSION['user'])): ?>
+                <a href="logout.php">Logout</a>
+            <?php else: ?>
+                <a href="login.php">Login</a>
+            <?php endif; ?>
+        </nav>
+    </header>
+
+    <!-- 
+        MAIN: Main content of the page.
+        The semantic element <main> is used.
+    -->
+
+    <main>
+        
+        <div class = "login-page">
+
+            <div class="login-box">
+                <h2>Login</h2>
+
+                <?php if($error): ?>
+                    <p class="error-msg"><?php echo $error; ?></p>
+                <?php endif; ?>
+
+                <form method="POST" class="login-form">
+                    <input type="email" name="email" placeholder="Enter your email" required>
+                    <input type="password" name="password" placeholder="Enter your password" required>
+                    <button type="submit">Login</button>
+                </form>
+
+                <p class="login-extra">
+                    Don’t have an account? <a href="register.php">Register</a>
+                </p>
+            </div>
+        </div>
+    </main>
+    <!-- 
+        FOOTER:
+        Semantic element <footer>
+        Contains contact information and useful links. (Copied entirely from the example video)
+    -->
+    <footer>
+        <div class="footer-col">
+            <h3>Links</h3>
+            <a href = "https://msuclanac.sharepoint.com/sites/CyprusStudentHub" > Student Union </a>
+        </div>
+
+        <div class="footer-col">
+            <h3>Contact</h3>
+            <p> Email: <a href = "mailto:suinformation@uclan.ac.uk">suinformation@uclan.ac.uk</a> <br></p>
+            <p> Tel: <a href = "01772893000">01772 89 3000</a> <br> </p>
+        </div>
+
+        <div class="footer-col">
+            <h3>Location</h3>
+            <p> University of Central Lancashire Students' union <br></p>
+            <p> Fylde road, Preston. PR1 7BY <br></p>
+            <p> Registered in England <br></p>
+            <p> Company Number: 7623917 <br></p>
+            <p> Registered Charity Number: 1142616 <br></p>
+        </div>
+    </footer>
+    <script src="../js/index.js"></script>
+</body>
+</html>
